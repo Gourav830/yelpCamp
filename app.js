b2a476373a951c66578350b0305c97eddaead399
“[WEb <gouravsingla830@gmail.com>
2024-03-07 14:38:12 +0530

    commit a pro

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const Campground = require('./models/capmgroung');
+const methodOverride = require('method-override');
+const cathcAsync = require('./utils/catchAsunc')
+const ExpressError = require('./utils/ExpressError');
+const { status } = require('init');
+app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+async function main()
+{
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp123');
+      console.log("CONNECTION OPEN to mongoose");
+
+}
+  main().catch(err => console.log("Error occuredd"));
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.engine('ejs',ejsMate);
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/campgrounds',async (req,res)=>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index',{campgrounds});
+
+})
+
+app.get('/campgrounds/new',(req,res)=>{
+    res.render('campgrounds/new')
+})
+
+
+app.get('/campgrounds/:id',cathcAsync(async (req,res)=>{
+   const campground = await Campground.findById(req.params.id);
+
+    res.render('campgrounds/show',{campground});
+}))
+app.get('/campgrounds/:id/edit',cathcAsync(async (req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+}))
+
+
+// app.get('/makeCampground', async (req,res) => {
+// const camp = new Campground({ title : 'MY BACKYARD',description: 'sasta version'});
+// await camp.save();
+// res.send(camp);
+
+// })
+app.post('/campgrounds',cathcAsync(async (req,res,next)=>{
+
+if(!req.body.campground)throw new ExpressError('Invalid camp DATA',400)
+const campground = new Campground(req.body.campground);
+await campground.save();
+res.redirect(`/campgrounds/${campground._id}`);
+
+next(e);
+})
+)
+app.put('/campgrounds/:id',cathcAsync(async (req,res,next)=>{
+
+    const { id } = req.params;
+
+  const campground132 =  await Campground.findByIdAndUpdate(id, { ...req.body.campground},{new :true});
+  res.redirect(`/campgrounds/${campground132._id}`);
+
+    next(e)
+}));
+
+app.delete('/campgrounds/:id',cathcAsync(async (req,res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+}))
+
+app.all('*',(req,res,next)=>{
+        next(new ExpressError('Page Not Found',404))
+})
+
+app.use((err,req,res,next)=>{
+// console.log(err);
+const{statusCode = 404} =err;
+if(!err.message) err.message = 'oh! No something went wrong!!';
+res.status(statusCode).render('error',{err})
+// next();
+
+})
+
+app.listen('1323',(req,res)=>{
+    console.log("Listning On 1323 !#@#");
+})
---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/capmgroung');
+const cities = require('./cities');
+const {places,descriptors} = require('./seedHelpers')
+async function main() 
+{
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp123');
+      console.log("CONNECTION OPEN to mongoose");
+  }
+ 
+  
+  main().catch(err => console.log("Error occuredd"));
+  
+  const sample = (array)=>    array[Math.floor(Math.random() * array.length)];
+  
+  const seedDB = async ()=>{
+    await Campground.deleteMany({});
+   for(let i=0;i<50;i++){
+    const price = Math.floor(Math.random()*40);
+    const random1000 = Math.floor(Math.random()*1000);
+  const newCamp =  new Campground({
+    location : `${cities[random1000].city},${cities[random1000].state}`,
+    title:`${sample(descriptors)} ${sample(places)}`,
+    image:'https://source.unsplash.com/collection/483251',
+    description : 'ipsum dolor sit amet consectetur adipisicing elit. Earum perferendis, aspernatur minima tempore facilis, expedita fuga suscipit quam ullam fugit, quidem nemo odit. Impedit ad fugiat cupiditate explicabo illum vitae.'
+,price
+   })
+   await newCamp.save();
+
+}
+  }
+
+  seedDB().then(()=>{
+    mongoose.connection.close();
+    console.log("EXITING....");
+  });
